7c60453d94652441b62a60a0c125ea3399887b75
Fazil-SA <fazilsa2000@gmail.com>
2025-11-17 19:09:54 +0300

    chatme ai chatbot

--- T5-AIChatbot/chatMe.py
+++ T5-AIChatbot/chatMe.py
@@ -1,7 +1,21 @@
 import gradio as gr
+from openai import OpenAI
 
+openai = OpenAI(base_url="http://localhost:11434/v1", api_key="ollama")
+
+# callback function for api
 def chat(message, history):
-    return f"Displaying the message itself since only ui integrated: {message}"
+    system_message = "You are a chatbot assistant"
+    history = [{"role": h["role"], "content": h["content"]} for h in history]
+    messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
+    response = openai.chat.completions.create(
+        model="llama3.2",
+        messages=messages,
+    )
+    return response.choices[0].message.content
 
+# gradio ui with function invoke
 gr.ChatInterface(fn=chat, type="messages").launch()
+
+
 
